4b70f8adf776b77eb9696d9f9e71e974f7f68553
zhaozhenghao1993 <736720794@qq.com>
2019-01-07 23:39:05 +0800

    update default language

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -7,7 +7,7 @@ const app = {
       withoutAnimation: false
     },
     device: 'desktop',
-    language: Cookies.get('language') || 'zh',
+    language: Cookies.get('language') || 'en',
     size: Cookies.get('size') || 'medium'
   },
   mutations: {
