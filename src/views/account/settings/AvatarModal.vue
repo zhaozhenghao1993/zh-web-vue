5e25cd5f5efbe53455620b2c407e1a45f67fd6b4
zhaozhenghao1993 <736720794@qq.com>
2019-05-21 21:54:24 +0800

    update avatar vue-cropper css

--- src/views/account/settings/AvatarModal.vue
+++ src/views/account/settings/AvatarModal.vue
@@ -10,19 +10,27 @@
       <a-col :xs="24" :md="12" :style="{height: '350px'}">
         <vue-cropper
           ref="cropper"
-          :img="options.img"
+          :img="option.img"
           :info="true"
-          :autoCrop="options.autoCrop"
-          :autoCropWidth="options.autoCropWidth"
-          :autoCropHeight="options.autoCropHeight"
-          :fixedBox="options.fixedBox"
+          :outputSize="option.size"
+          :outputType="option.outputType"
+          :full="option.full"
+          :canMove="option.canMove"
+          :canMoveBox="option.canMoveBox"
+          :original="option.original"
+          :autoCrop="option.autoCrop"
+          :autoCropWidth="option.autoCropWidth"
+          :autoCropHeight="option.autoCropHeight"
+          :fixedBox="option.fixedBox"
           @realTime="realTime"
         >
         </vue-cropper>
       </a-col>
       <a-col :xs="24" :md="12" :style="{height: '350px'}">
-        <div class="avatar-upload-preview">
-          <img :src="previews.url" :style="previews.img"/>
+        <div :style="{'width': previews.w + 'px', 'height': previews.h + 'px'}" class="avatar-upload-preview">
+          <div :style="previews.div" class="preview">
+            <img :src="previews.url" :style="previews.img">
+          </div>
         </div>
       </a-col>
     </a-row>
@@ -31,7 +39,7 @@
       <label class="btn" for="uploads">upload</label>
       <input type="file" id="uploads" style="position:absolute; clip:rect(0 0 0 0);" accept="image/png, image/jpeg, image/gif, image/jpg" @change="uploadImg($event, 1)">
       <a-button key="back" @click="cancelHandel">取消</a-button>
-      <a-button key="submit" type="primary" :loading="confirmLoading" @click="okHandel">保存</a-button>
+      <a-button key="submit" type="primary" :loading="confirmLoading" @click="okHandel('blob')">保存</a-button>
     </template>
   </a-modal>
 </template>
@@ -48,8 +56,14 @@ export default {
       id: null,
       confirmLoading: false,
 
-      options: {
+      option: {
         img: '/avatar2.jpg',
+        outputSize: 1, // 剪切后的图片质量（0.1-1）
+        full: false, // 输出原图比例截图 props名full
+        outputType: 'png',
+        canMove: true,
+        original: false,
+        canMoveBox: true,
         autoCrop: true,
         autoCropWidth: 200,
         autoCropHeight: 200,
@@ -71,28 +85,58 @@ export default {
     cancelHandel () {
       this.close()
     },
-    okHandel () {
-      const vm = this
+    okHandel (type) {
+      /* const vm = this
 
       vm.confirmLoading = true
       setTimeout(() => {
         vm.confirmLoading = false
         vm.close()
         vm.$message.success('上传头像成功')
-      }, 2000)
+      }, 2000) */
+      console.log('finish')
+      const formData = new FormData()
+      // 输出
+      if (type === 'blob') {
+        this.$refs.cropper.getCropBlob((data) => {
+          const img = window.URL.createObjectURL(data)
+          this.model = true
+          this.modelSrc = img
+          console.log('data', data)
+          formData.append('file', data, this.fileName)
+          /* this.$http.post(Api.uploadSysHeadImg.url, formData, { contentType: false, processData: false, headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
+            .then((response) => {
+              var res = response.data
+              if (res.success === 1) {
+                _this.imgFile = ''
+                _this.headImg = res.realPathList[0] // 完整路径
+                _this.uploadImgRelaPath = res.relaPathList[0] // 非完整路径
+                _this.$message({ // element-ui的消息Message消息提示组件
+                  type: 'success',
+                  message: '上传成功'
+                })
+              }
+            }) */
+        })
+      } else {
+        this.$refs.cropper.getCropData((data) => {
+          this.model = true
+          this.modelSrc = data
+        })
+      }
     },
     // 选择本地图片
     uploadImg (e, num) {
       console.log('uploadImg')
-      var _this = this
+      const _this = this
       // 上传图片
-      var file = e.target.files[0]
+      const file = e.target.files[0]
       _this.fileName = file.name
       if (!/\.(gif|jpg|jpeg|png|bmp|GIF|JPG|PNG)$/.test(e.target.value)) {
-        alert('图片类型必须是.gif,jpeg,jpg,png,bmp中的一种')
+        this.$message.error('图片类型必须是.gif,jpeg,jpg,png,bmp中的一种')
         return false
       }
-      var reader = new FileReader()
+      const reader = new FileReader()
       reader.onload = (e) => {
         let data
         if (typeof e.target.result === 'object') {
@@ -102,6 +146,7 @@ export default {
           data = e.target.result
         }
         if (num === 1) {
+          console.log('data', data)
           _this.option.img = data
         } else if (num === 2) {
           _this.example2.img = data
@@ -113,7 +158,6 @@ export default {
       reader.readAsArrayBuffer(file)
     },
     realTime (data) {
-      console.log(11)
       this.previews = data
     }
   }
@@ -123,14 +167,12 @@ export default {
 <style lang="less" scoped>
 
   .avatar-upload-preview {
-    position: absolute;
-    top: 50%;
-    transform: translate(50%, -50%);
-    width: 180px;
-    height: 180px;
-    border-radius: 50%;
-    box-shadow: 0 0 4px #ccc;
     overflow: hidden;
+    margin: 5px;
+    border-radius: 50%;
+    position: absolute;
+    box-shadow: 0 0 4px #ccc;
+    transform: translate3d(88px, 75px, 0px);
 
     img {
       width: 100%;
